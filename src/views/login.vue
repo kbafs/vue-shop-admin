54050f386e5dd9ca0070e56b8a644b1a7841d7ff
gengjiaxiang <gegnjiaxiang@alibaba.com>
2019-06-28 12:37:09 +0800

    登录功能完成

--- src/views/login.vue
+++ src/views/login.vue
@@ -2,16 +2,23 @@
   <!--el-row :代表行：一行，el-col：代表列，一列，用flex布局 ，el-form：一列里的内容，就是整个登录框 -->
   <el-row type="flex" class="row-bg" justify="center" align="middle">
     <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6">
-      <el-form ref="form" :model="form" label-position="top" label-width="80px" class="login-form">
-        <el-form-item label="用户名" prop="region">
-          <el-input v-model="form.name" prop="username"></el-input>
+      <el-form
+        ref="loginForm"
+        :model="form"
+        :rules="rules"
+        label-position="top"
+        label-width="80px"
+        class="login-form"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="form.username"></el-input>
         </el-form-item>
-        <el-form-item label="密码" prop="region">
-          <el-input v-model="form.password" prop="password"></el-input>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="form.password"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">登录</el-button>
-          <el-button>重置</el-button>
+          <el-button type="primary" @click="submitForm('loginForm')">登录</el-button>
+          <el-button @click="resetForm('loginForm')">重置</el-button>
         </el-form-item>
       </el-form>
     </el-col>
@@ -24,14 +31,43 @@ export default {
   data() {
     return {
       form: {
-        name: "",
+        username: "",
         password: ""
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "change" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 8, message: "长度在 6 到 8 个字符", trigger: "change" }
+        ]
       }
     };
   },
   methods: {
-    onSubmit() {
-      console.log("submit!");
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          axios({
+            url: "http://localhost:8888/api/private/v1/login",
+            method: "post",
+            data: this.form
+          }).then(res => {
+            if (res.data.meta.status == 200) {
+              localStorage.setItem("token", data.token);
+              this.$router.push("/home");
+            }
+          });
+        } else {
+          // console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
     }
   }
 };
